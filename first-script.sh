6afeee28e0c7b5da4cc7279800cfc37a42620a6c
astanil <astakhovnil@gmail.com>
2022-08-31 12:54:06 +0300

    First commit

---
+++ first-script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#installing dependencies
+sudo apt-get install wget gpg
+
+#importing gpg key
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+
+#enabling repo
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+
+#installing code
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code
+
+#checking availability 
+if ! command -v code &> /dev/null
+then
+    echo "VS isn't installed"
+    exit
+else
+    echo "VS is installed"
+fi
